bedfb6d876a18c4a78e8c105dc73ad5ae29b3c11
Keyrxng <106303466+Keyrxng@users.noreply.github.com>
2024-05-23 20:54:30 +0100

    fix: add config defaults

--- src/types/plugin-input.ts
+++ src/types/plugin-input.ts
@@ -10,19 +10,35 @@ export interface PluginInputs<T extends SupportedEventsU = SupportedEventsU, TU 
   ref: string;
 }
 
+const oneDay = 24 * 60 * 60 * 1000;
+
 export const startStopSchema = T.Object({
-  disabledCommands: T.Array(T.String()),
-  labels: T.Object({
-    time: T.Array(T.String()),
-    priority: T.Array(T.String()),
-  }),
-  timers: T.Object({
-    reviewDelayTolerance: T.Number(),
-    taskStaleTimeoutDuration: T.Number(),
-  }),
-  miscellaneous: T.Object({
-    maxConcurrentTasks: T.Number(),
-  }),
+  disabledCommands: T.Array(T.String(), { default: ["start"] }),
+  labels: T.Object(
+    {
+      time: T.Array(T.String()),
+      priority: T.Array(T.String()),
+    },
+    {
+      default: {
+        time: ["Time: <1 hour", "Time: <2 hours", "Time: <4 hours", "Time: <1 day", "Time: <2 days", "Time: <1 week"],
+        priority: ["Priority: 1 (Normal)", "Priority: 2 (Medium)", "Priority: 3 (high)", "Priority: 4 (Urgent)", "Priority: 5 (Emergency)"],
+      },
+    }
+  ),
+  timers: T.Object(
+    {
+      reviewDelayTolerance: T.Number(),
+      taskStaleTimeoutDuration: T.Number(),
+    },
+    { default: { reviewDelayTolerance: oneDay * 5, taskStaleTimeoutDuration: oneDay * 30 } }
+  ),
+  miscellaneous: T.Object(
+    {
+      maxConcurrentTasks: T.Number(),
+    },
+    { default: { maxConcurrentTasks: 3 } }
+  ),
 });
 
 export type StartStopSettings = StaticDecode<typeof startStopSchema>;
