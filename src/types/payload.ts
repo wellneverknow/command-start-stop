941c0fe4395ecd8f0c76b8330981e1bf06c558a1
Keyrxng <106303466+Keyrxng@users.noreply.github.com>
2024-06-11 07:10:00 +0100

    chore: remove unused type

--- src/types/payload.ts
+++ src/types/payload.ts
@@ -1,7 +1,6 @@
 import { LogReturn } from "../adapters/supabase/helpers/logs";
 import { RestEndpointMethodTypes } from "@octokit/rest";
 
-export type User = RestEndpointMethodTypes["users"]["getByUsername"]["response"]["data"];
 export type Issue = RestEndpointMethodTypes["issues"]["get"]["response"]["data"];
 export type Label = RestEndpointMethodTypes["issues"]["listLabelsOnIssue"]["response"]["data"][0];
 export type HandlerReturnValuesNoVoid = null | string | LogReturn;
