b166046fd354af75fdd0052455663e45898993ee
Keyrxng <106303466+Keyrxng@users.noreply.github.com>
2024-06-24 16:00:18 +0100

    fix: removing helpers previously missed

--- src/adapters/supabase/helpers/user.ts
+++ src/adapters/supabase/helpers/user.ts
@@ -12,7 +12,7 @@ export class User extends Super {
   async getWalletByUserId(userId: number, issueNumber: number) {
     const { data, error } = await this.supabase.from("users").select("wallets(*)").eq("id", userId).single();
     if ((error && !data) || !data.wallets?.address) {
-      this.context.logger.error("No wallet address found", { userId, issueNumber }, true);
+      this.context.logger.error("No wallet address found", { userId, issueNumber });
       if (this.context.config.miscellaneous.startRequiresWallet) {
         await addCommentToIssue(this.context, "```diff\n! Please set your wallet address with the /wallet command first and try again.\n```");
         throw new Error("No wallet address found");
@@ -24,40 +24,5 @@ export class User extends Super {
     }
 
     return data?.wallets?.address || null;
-  }
-
-  public async getMultiplier(userId: number, repositoryId: number) {
-    const locationData = await this.getLocationsFromRepo(repositoryId);
-    if (locationData && locationData.length > 0) {
-      const accessData = await this._getAccessData(locationData, userId);
-      if (accessData) {
-        return {
-          value: accessData.multiplier || null,
-          reason: accessData.multiplier_reason || null,
-        };
-      }
-    }
-    return null;
-  }
-
-  private async _getAccessData(locationData: { id: number }[], userId: number) {
-    const locationIdsInCurrentRepository = locationData.map((location) => location.id);
-
-    const { data: accessData, error: accessError } = await this.supabase
-      .from("access")
-      .select("multiplier, multiplier_reason")
-      .in("location_id", locationIdsInCurrentRepository)
-      .eq("user_id", userId)
-      .order("id", { ascending: false }) // get the latest one
-      .maybeSingle();
-    if (accessError) throw this.context.logger.error("Error getting access data", accessError);
-    return accessData;
-  }
-
-  public async getLocationsFromRepo(repositoryId: number) {
-    const { data: locationData, error } = await this.supabase.from("locations").select("id").eq("repository_id", repositoryId);
-
-    if (error) throw this.context.logger.error("Error getting location data", new Error(error.message));
-    return locationData;
   }
 }
