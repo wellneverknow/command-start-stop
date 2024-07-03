c254df01fb414dc66240d268e47994d86022167a
Keyrxng <106303466+Keyrxng@users.noreply.github.com>
2024-07-03 23:40:25 +0100

    chore: use slashCommand syntax

--- src/handlers/user-start-stop.ts
+++ src/handlers/user-start-stop.ts
@@ -6,18 +6,18 @@ import { stop } from "./shared/stop";
 export async function userStartStop(context: Context): Promise<{ output: string | null }> {
   const { payload, config } = context;
   const { issue, comment, sender, repository } = payload;
-  const directive = comment.body.split(" ")[0].replace("/", "");
+  const slashCommand = comment.body.split(" ")[0].replace("/", "");
   const { disabledCommands } = config;
-  const isCommandDisabled = disabledCommands.some((command: string) => command === directive);
+  const isCommandDisabled = disabledCommands.some((command: string) => command === slashCommand);
 
   if (isCommandDisabled) {
     await addCommentToIssue(context, "```diff\n! The /start command is disabled for this repository.\n```");
-    throw new Error(`The '/${directive}' command is disabled for this repository.`);
+    throw new Error(`The '/${slashCommand}' command is disabled for this repository.`);
   }
 
-  if (directive === "stop") {
+  if (slashCommand === "stop") {
     return await stop(context, issue, sender, repository);
-  } else if (directive === "start") {
+  } else if (slashCommand === "start") {
     return await start(context, issue, sender);
   }
 
