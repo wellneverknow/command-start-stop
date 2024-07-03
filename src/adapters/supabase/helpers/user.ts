ecb4d97510aa81a824a1fb0218c91990ae611290
Keyrxng <106303466+Keyrxng@users.noreply.github.com>
2024-07-04 00:18:48 +0100

    chore: remove database.ts

--- src/adapters/supabase/helpers/user.ts
+++ src/adapters/supabase/helpers/user.ts
@@ -1,16 +1,19 @@
-import { Database } from "../types/database";
 import { SupabaseClient } from "@supabase/supabase-js";
 import { Super } from "./supabase";
 import { Context } from "../../../types/context";
 import { addCommentToIssue } from "../../../utils/issue";
 
+type Wallet = {
+  address: string;
+};
+
 export class User extends Super {
-  constructor(supabase: SupabaseClient<Database>, context: Context) {
+  constructor(supabase: SupabaseClient, context: Context) {
     super(supabase, context);
   }
 
   async getWalletByUserId(userId: number, issueNumber: number) {
-    const { data, error } = await this.supabase.from("users").select("wallets(*)").eq("id", userId).single();
+    const { data, error } = await this.supabase.from("users").select("wallets(*)").eq("id", userId).single() as { data: { wallets: Wallet }, error: unknown };
     if ((error && !data) || !data.wallets?.address) {
       this.context.logger.error("No wallet address found", { userId, issueNumber });
       if (this.context.config.miscellaneous.startRequiresWallet) {
