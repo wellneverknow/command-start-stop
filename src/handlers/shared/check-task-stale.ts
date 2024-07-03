07f01810564f2deadc48c90262fcdd2681490043
Keyrxng <106303466+Keyrxng@users.noreply.github.com>
2024-07-03 23:36:43 +0100

    chore: refactor checkStaleTask

--- src/handlers/shared/check-task-stale.ts
+++ src/handlers/shared/check-task-stale.ts
@@ -1,12 +1,9 @@
 export function checkTaskStale(staleTask: number, createdAt: string) {
-  let days: number | undefined;
-  let staleToDays: number | undefined;
-  let isTaskStale = false;
-
   if (staleTask !== 0) {
-    days = Math.floor((new Date().getTime() - new Date(createdAt).getTime()) / (1000 * 60 * 60 * 24));
-    staleToDays = Math.floor(staleTask / (1000 * 60 * 60 * 24));
-    isTaskStale = days >= staleToDays;
+    const days = Math.floor((new Date().getTime() - new Date(createdAt).getTime()) / (1000 * 60 * 60 * 24));
+    const staleToDays = Math.floor(staleTask / (1000 * 60 * 60 * 24));
+    return days >= staleToDays;
   }
-  return isTaskStale;
+
+  return false;
 }
