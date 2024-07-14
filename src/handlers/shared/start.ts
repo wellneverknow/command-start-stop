8d5c56a65c6f1fb586fd1d1d518c876db5516f47
jordan-ae <arreyetta1jrn@gmail.com>
2024-07-14 22:53:00 +0100

    feat: max task assignment for collaborators

--- src/handlers/shared/start.ts
+++ src/handlers/shared/start.ts
@@ -1,6 +1,7 @@
 import { Assignee, Context, ISSUE_TYPE, Label } from "../../types";
 import { isParentIssue, getAvailableOpenedPullRequests, getAssignedIssues, addAssignees, addCommentToIssue, getTimeValue } from "../../utils/issue";
 import { calculateDurations } from "../../utils/shared";
+import { isUserMember } from "./check-org-member";
 import { checkTaskStale } from "./check-task-stale";
 import { generateAssignmentComment } from "./generate-assignment-comment";
 import { getUserRoleAndTaskLimit } from "./get-user-task-limit-and-role";
