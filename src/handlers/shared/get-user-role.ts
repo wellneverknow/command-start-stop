391bdde81d1f1da7b6d9f95ec7e445865024e271
jordan-ae <arreyetta1jrn@gmail.com>
2024-07-23 15:50:23 +0100

    fix: return lowest task limit if user role doesnt match any of those in the config

--- src/handlers/shared/get-user-role.ts
+++ src/handlers/shared/get-user-role.ts
@@ -2,19 +2,26 @@ import { Context } from "../../types";
 
 export async function getUserRole(context: Context, user: string): Promise<string> {
   const orgLogin = context.payload.organization?.login;
+  const { config } = context;
+  const { maxConcurrentTasks } = config.miscellaneous;
 
   if (!orgLogin) {
     throw new Error("Organization login not found in context payload.");
   }
 
-  try {
-    const response = await context.octokit.orgs.getMembershipForUser({
-      org: orgLogin,
-      username: user,
-    });
+  const response = await context.octokit.orgs.getMembershipForUser({
+    org: orgLogin,
+    username: user,
+  });
 
-    return response.data.role;
-  } catch (error) {
-    return "contributor";
+  const matchingUser = maxConcurrentTasks.find(({ role }) => role.toLowerCase() === response.data.role);
+
+  if (matchingUser) {
+    //chech if the current user role matches any of those defined in the config
+    return matchingUser.role;
+  } else {
+    //return the role with the smallest task limit
+    const minLimitTask = maxConcurrentTasks.reduce((minTask, currentTask) => (currentTask.limit < minTask.limit ? currentTask : minTask));
+    return minLimitTask.role;
   }
 }
