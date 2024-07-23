bf6e7da529cdf7ccaa3c8b788d2dd35a15e8181f
jordan-ae <arreyetta1jrn@gmail.com>
2024-07-23 18:01:55 +0100

    chore: code cleanups

--- src/handlers/shared/get-user-role.ts
+++ src/handlers/shared/get-user-role.ts
@@ -1,27 +1,31 @@
 import { Context } from "../../types";
 
-export async function getUserRole(context: Context, user: string): Promise<string> {
+interface MatchingUserProps {
+  role: string;
+  limit: number;
+}
+
+export async function getUserRole(context: Context, user: string): Promise<MatchingUserProps> {
   const orgLogin = context.payload.organization?.login;
-  const { config } = context;
+  const { config, logger } = context;
   const { maxConcurrentTasks } = config.miscellaneous;
 
-  if (!orgLogin) {
-    throw new Error("Organization login not found in context payload.");
-  }
+  try {
+    const response = await context.octokit.orgs.getMembershipForUser({
+      org: orgLogin as string,
+      username: user,
+    });
 
-  const response = await context.octokit.orgs.getMembershipForUser({
-    org: orgLogin,
-    username: user,
-  });
+    const matchingUser = maxConcurrentTasks.find(({ role }) => role.toLowerCase() === response.data.role);
 
-  const matchingUser = maxConcurrentTasks.find(({ role }) => role.toLowerCase() === response.data.role);
-
-  if (matchingUser) {
-    //chech if the current user role matches any of those defined in the config
-    return matchingUser.role;
-  } else {
-    //return the role with the smallest task limit
-    const minLimitTask = maxConcurrentTasks.reduce((minTask, currentTask) => (currentTask.limit < minTask.limit ? currentTask : minTask));
-    return minLimitTask.role;
+    if (matchingUser) {
+      //chech if the current user role matches any of those defined in the config
+      return matchingUser;
+    } else {
+      //return the role with the smallest task limit
+      return maxConcurrentTasks.reduce((minTask, currentTask) => (currentTask.limit < minTask.limit ? currentTask : minTask));
+    }
+  } catch (error) {
+    logger.error("An error occured", { error: error as Error });
   }
 }
