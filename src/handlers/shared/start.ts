8221f51f3298438b3c9d5e64f22c39a248f94050
jordan-ae <arreyetta1jnr@gmail.com>
2024-08-06 19:09:07 +0100

    fix: use logger to display message

--- src/handlers/shared/start.ts
+++ src/handlers/shared/start.ts
@@ -48,7 +48,8 @@ export async function start(context: Context, issue: Context["payload"]["issue"]
   // check for max and enforce max
 
   if (Math.abs(assignedIssues.length - openedPullRequests.length) >= maxTask.limit) {
-    await addCommentToIssue(context, "```diff\n! Too many assigned issues, you have reached your max limit.\n```");
+    const log = logger.error("Too many assigned issues, you have reached your max limit")
+    await addCommentToIssue(context, log?.logMessage.diff as string);
     throw new Error(`Too many assigned issues, you have reached your max limit of ${maxTask.limit} issues.`);
   }
 
