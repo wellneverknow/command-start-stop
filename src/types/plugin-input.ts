a5bd14dac37ee612e257e3a2d0e4375cc5f8b373
jordan-ae <arreyetta1jnr@gmail.com>
2024-08-15 15:54:09 +0100

    fix: user t.record and loop by object.entries

--- src/types/plugin-input.ts
+++ src/types/plugin-input.ts
@@ -1,7 +1,6 @@
 import { SupportedEvents, SupportedEventsU } from "./context";
 import { Static, Type as T } from "@sinclair/typebox";
 import { StandardValidator } from "typebox-validators";
-import { validateSchemaForDuplicateRoles } from "../utils/validate-schema-for-duplicate-roles";
 
 export interface PluginInputs<T extends SupportedEventsU = SupportedEventsU, TU extends SupportedEvents[T] = SupportedEvents[T]> {
   stateId: string;
