2f6703d7f2db28e8d7e416b29dc617a4f1cc5db8
Mentlegen <9807008+gentlementlegen@users.noreply.github.com>
2024-09-11 12:43:33 +0900

    Update context.ts

--- src/types/context.ts
+++ src/types/context.ts
@@ -13,7 +13,7 @@ export type SupportedEvents = {
 };
 
 export function isContextCommentCreated(context: Context): context is Context<"issue_comment.created"> {
-  return "comment" in context.payload;
+  return "issue" in context.payload;
 }
 
 export interface Context<T extends SupportedEventsU = SupportedEventsU, TU extends SupportedEvents[T] = SupportedEvents[T]> {
